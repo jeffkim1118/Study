a826d234297e5032b970496009244ebe03256c91
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-09-23 13:07:46 -0400

    Added coding problems to the file

--- test.js
+++ test.js
@@ -1,19 +1,25 @@
 /*
-print out numbers in arrays
+Create a function to print out numbers in arrays.
+The function will be called three times.
+The numbers must be in sequential order.
+Don't have to print out numbers that are already been printed out.
+
+example:
 
 function call 1
-[2, 5]
+input: [2, 5]
 prints out: nothing.
 
 function call 2
-[1]
+input: [1]
 prints out: 1, 2
 
 function call 3
-[3,4,7,6]
-prints out: 
+input: [3,4,7,6]
+prints out: 3, 4, 5, 6, 7
 */
 
+//My Solution
 function printArray(array){
     let newArray = array.slice()
     let sortedArray = newArray.sort((a,b) => a - b)
@@ -26,3 +32,22 @@ function printArray(array){
 printArray([3, 4, 1])
 
 
+
+/*
+Build a function to find the minimum amount of obstacles that need to be destroyed
+to make the blocks to reach the bottom.
+
+* = block
+0 = empty space
+X = obstacle
+
+example:
+Input:
+[[0, *, *], 
+ [*, *, 0], 
+ [*, *, 0],
+ [X, 0, X],
+ [0, X, 0]]
+
+ Output: 2
+*/
