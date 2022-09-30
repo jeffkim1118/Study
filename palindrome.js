d5ff05631c6341571037c82c55147428425f14b5
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-09-29 22:09:46 -0400

    Added more solutions for problems from hackkerrank

--- palindrome.js
+++ palindrome.js
@@ -1,22 +1,50 @@
-function palindrome(str) {
-    let filteredStr = str.replace(/[^a-z]/gi,'').toLowerCase();
-    let letters = [];
-    let reverseStr = "";
-    console.log(filteredStr)
+// function palindrome(str) {
+//     let filteredStr = str.replace(/[^a-z]/gi,'').toLowerCase();
+//     let letters = [];
+//     let reverseStr = "";
+//     console.log(filteredStr)
   
-    for(let i = 0; i < filteredStr.length; i++){
-      letters.push(filteredStr[i]);
-    }
+//     for(let i = 0; i < filteredStr.length; i++){
+//       letters.push(filteredStr[i]);
+//     }
   
-    for(let i = 0; i < filteredStr.length; i++){
-      reverseStr += letters.pop();
-    }
+//     for(let i = 0; i < filteredStr.length; i++){
+//       reverseStr += letters.pop();
+//     }
   
-    if(reverseStr === filteredStr){
-      console.log(true);
-    }else{
-        console.log(false);
-    }
-  }
+//     if(reverseStr === filteredStr){
+//       console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+//   }
   
-  palindrome("My age is 0, 0 si ega ym.");
+//   palindrome("My age is 0, 0 si ega ym.");
+
+
+
+//   function isPalindrome(x) {   
+//     let stringNum = x.toString()
+//     let arr = []
+
+//     for(let i = stringNum.length-1; i>=0 ; i--){
+//       arr.push(stringNum[i])
+//     }
+//     let reversed = arr.join('')
+//     let reversedNum = parseInt(reversed)
+    
+//     if(reversedNum === x){
+//       return true
+//     }else{
+//       return false
+//     }
+// };
+
+var isPalindrome = function(x) {
+  let arr = new Array()
+  arr.push(x)
+  console.log(arr.join(''))
+  // console.log( arr.join('').split('').reverse().join('') === arr.join())
+  };
+
+isPalindrome(131)
--- test.js
+++ test.js
@@ -20,37 +20,37 @@ prints out: 3, 4, 5, 6, 7
 */
 
 //My Solution
-function buildFunction(){
-    const data = {
-        input: [],
-        output: []
-    }
-    return (numbers) => {
-        data.input = data.input.concat(numbers)
-        data.input.sort()
-        data.input.forEach(n => {
-            if(data.output.includes(n)){
-                return
-            }
+// function buildFunction(){
+//     const data = {
+//         input: [],
+//         output: []
+//     }
+//     return (numbers) => {
+//         data.input = data.input.concat(numbers)
+//         data.input.sort()
+//         data.input.forEach(n => {
+//             if(data.output.includes(n)){
+//                 return
+//             }
 
-            if(n === 1){
-                data.output.push(n)
-                console.log(n)
-            }
+//             if(n === 1){
+//                 data.output.push(n)
+//                 console.log(n)
+//             }
 
-            let printMe = true
-            for(let i = n - 1; i >= 1; i++){
-                if(i > 1 && !data.output.includes(i)){
-                    printMe = false
-                    break
-                }
-            }
+//             let printMe = true
+//             for(let i = n - 1; i >= 1; i++){
+//                 if(i > 1 && !data.output.includes(i)){
+//                     printMe = false
+//                     break
+//                 }
+//             }
 
-            if(printMe){
-                data.output.push(n)
-                console.log(n)
-            }
-        })
+//             if(printMe){
+//                 data.output.push(n)
+//                 console.log(n)
+//             }
+//         })
         // let max = 0
         // data.input.forEach(n=>{
         //     if(n>max){
@@ -66,13 +66,13 @@ function buildFunction(){
         //     }
         // }
         // console.log(forOutPut.join(", "))
-    }
-}
-const printArray = buildFunction()
+//     }
+// }
+// const printArray = buildFunction()
 
-printArray([2, 5])
-printArray([1])
-printArray([3, 4, 7, 6])
+// printArray([2, 5])
+// printArray([1])
+// printArray([3, 4, 7, 6])
 
 // function printArray(array){
 //     let newArray = array.slice()
@@ -105,3 +105,102 @@ Input:
 
  Output: 2
 */
+
+
+// function viralAdvertising(n) {
+//     let shared = 5
+//     let liked = 0 
+//     let cumulative = 0
+    
+//     for(let day = 0; day < n; day++){
+//         liked = Math.floor(shared/2)
+//         // console.log(liked)
+//         cumulative += liked
+//         // console.log(cumulative)
+//         shared = liked * 3
+//     }
+//     console.log(cumulative)
+// }
+
+// viralAdvertising(3)
+
+// const queries = [ 1, 2, 5 ]
+
+// for(let i of queries){
+//     console.log(i)
+// }
+
+
+// function buildPrint() {
+//     // This is the data the inner function will close over, allowing it to remember things across calls
+//     const data = {input: [], output: []}
+  
+//     return (numbers) => {
+//       // First, we need to add the new numbers to our full list of inputs
+//       data.input = data.input.concat(numbers)
+  
+//       // Then we find the maximum number we've been given so far
+//       let max = data.input[0]
+//       data.input.forEach(n  => {
+//         if (n > max) {
+//           max = n
+//         }
+//       })
+  
+//       // Now, we loop from 1 to the maximum number we've been given
+//       const toPrint = []
+//       for (let i = 1; i <= max; i++) {
+  
+//         // if we find a number we haven't been given yet, we stop
+//         if (!data.input.includes(i)){
+//           break;
+//         }
+  
+//         // if we haven't output the number yet, we add it to the array of numbers to be printed this call
+//         if (!data.output.includes(i)) {
+//           toPrint.push(i)
+//         }
+//       }
+  
+//       // we add all the numbers we'll be printing to our log of output numbers and then print them out
+//       data.output = data.output.concat(toPrint)
+//       console.log(toPrint.join(', '))
+//     }
+//   }
+  
+//   const print = buildPrint()
+  
+//   console.log('call 1 with [2, 5]')
+//   print([2, 5])
+//   console.log('call 2 with [1]')
+//   print([1])
+//   console.log('call 3 with [3, 4, 7, 6]')
+//   print([3, 4, 7, 6])
+
+
+// let n = []
+// for(let i = 1; i <=p.length; i++){
+//     let ind = p.indexOf(i) + 1
+//     n.push(ind) 
+// }
+// let res = []
+// for (let num of n){
+//    res.push(p.indexOf(num)+1)
+// }
+// return res
+
+
+function jumpingOnClouds(c, k) {
+    let e = 100; let index = []
+    for(let i = 0; i < c.length; i++){
+        if(c[i] == 1){
+            e -= 2;
+        }
+        e--;
+    }
+    console.log(e);
+}
+
+let c = [0, 0, 1, 0, 0, 1, 1, 0]
+let k = 2
+jumpingOnClouds(c, k)
