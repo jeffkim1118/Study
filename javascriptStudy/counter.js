12dd468e27fb7479f6a15e1eab4e9c02b54e540d
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-10-28 17:00:06 -0400

    commented out the solutions

--- javascriptStudy/counter.js
+++ javascriptStudy/counter.js
@@ -3,19 +3,21 @@ let arr = ['apple', 'orange', 'banana', 'watermelon', 'grapes']
 
 let charCounter = 0;
 
-for(const fruit of arr){
-  for(const char in fruit){
-    charCounter+=1
-  }
-  if(charCounter > 5){
-    console.log(true);
-    charCounter = 0;
-  }else{
-    console.log(false)
-    charCounter = 0;
-  }
-}
+//First way
+// for(const fruit of arr){
+//   for(const char in fruit){
+//     charCounter+=1
+//   }
+//   if(charCounter > 5){
+//     console.log(true);
+//     charCounter = 0;
+//   }else{
+//     console.log(false)
+//     charCounter = 0;
+//   }
+// }
 
+//Second way
 // for(const fruit of arr){
 //   fruit.split('').map((char) => {charCounter++})
 //   if(charCounter > 5){
