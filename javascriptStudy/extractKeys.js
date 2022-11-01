dc2a57fa21e58a35eec16fdf5da5d9e3bf6e2160
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-10-31 20:12:26 -0400

    new files added

---
+++ javascriptStudy/extractKeys.js
@@ -0,0 +1,10 @@
+// Write a function that takes an object (a) as argument
+// Return an array with all object keys
+
+function myFunction(a){
+    console.log(Object.keys(a))
+}
+
+myFunction({a:1,b:2,c:3})
+myFunction({j:9,i:2,x:3,z:4})
+myFunction({w:15,x:22,y:13})
