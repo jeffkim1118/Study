4ca2ced6d90d1219e8efd7edd969a41802275b1d
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-11-02 16:34:58 -0400

    updated reverse string file

--- javascriptStudy/reverseString.js
+++ javascriptStudy/reverseString.js
@@ -1,9 +1,11 @@
 // Reverse a given string but characters inside of "{}" must not be reversed.
 
-// function reverseString(s){
+function reverseString(s){
     
-// }
-// reverseString('abcd{ef}ghij')  // expect: 'jihg{ef}dcba'
+}
+reverseString('abcd{ef}ghij')  // expect: 'jihg{ef}dcba'
+
+
 
 
 // Reverse words in a string
@@ -14,21 +16,24 @@
 // }
 // reverseWords("Let's take Leetcode contest")
 
-let nums1 = [1,2,2,1]
-let nums2 = [2,2]
+// let nums1 = [1,2,2,1]
+// let nums2 = [2,2]
 
-function intersection(nums1, nums2){
-    const set1 = new Set(nums1);
-    const set2 = new Set(nums2)
-    console.log(set1)
-    console.log(set2)
-    // const set2 = new Set(nums2);
-    // const set3 = new Set();
-    // for (const val of set1) {
-    //     if (set2.has(val)) {
-    //         set3.add(val);
-    //     }
-    // }
-    // console.log(Array.from(set3));
-}
-intersection(nums1, nums2)
+// function intersection(nums1, nums2){
+//     const set1 = new Set(nums1);
+//     const set2 = new Set(nums2)
+//     console.log(set1)
+//     console.log(set2)
+
+
+
+//     // const set2 = new Set(nums2);
+//     // const set3 = new Set();
+//     // for (const val of set1) {
+//     //     if (set2.has(val)) {
+//     //         set3.add(val);
+//     //     }
+//     // }
+//     // console.log(Array.from(set3));
+// }
+// intersection(nums1, nums2)
