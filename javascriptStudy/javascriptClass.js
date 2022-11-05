ecc8043ccd555c3247d6577d2dd0720e5e54ddc4
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-11-05 18:38:56 -0400

    javascript class study file added

---
+++ javascriptStudy/javascriptClass.js
@@ -0,0 +1,19 @@
+class Person {
+    name;
+    age;
+    gender;
+
+    constructor(name, age, gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    
+    introduce(){
+        console.log(`Hi! ${this.name}. Age: ${this.age}, gender: ${this.gender}`)
+    }
+
+}
+
+const kim = new Person('Kim', 25, 'Male');
+kim.introduce();
