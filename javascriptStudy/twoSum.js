9922061d6392ada6eb3bff5846ecc1392ccb568b
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-11-09 21:21:32 -0500

    two sum question new solution added

--- javascriptStudy/twoSum.js
+++ javascriptStudy/twoSum.js
@@ -8,15 +8,17 @@ var twoSum = function(nums,target) {
     //          }
     //      }
     //  }
-
+    let ans = [];
     const chart = {};
     for(let i = 0; i < nums.length; i++){
-        let temp = nums[i];
-        chart[temp] = i;
-        if(chart[target-temp] >= 0){
-            return [chart[target-temp], i]
+        if(chart[target - nums[i]] !== undefined){
+            ans[0] = parseInt(chart[target-nums[i]]);
+            ans[1] = i;
+            return ans
         }
+        chart[nums[i]] = i
     }
  };
  
 console.log(twoSum([2,7,11,45], 9))
+console.log(twoSum([3,2,4], 6))
