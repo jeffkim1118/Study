b255997667354de87acace26eb8cf9b3b0972361
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-11-11 22:22:23 -0500

    tried out every method in javascript

---
+++ javascriptStudy/every.js
@@ -0,0 +1,5 @@
+const numbers = [11, 14, 11, 23, 53, 77]
+
+const filter = (number) => number > 10;
+
+console.log(numbers.every(filter))
--- javascriptStudy/linkedList.js
+++ javascriptStudy/linkedList.js
@@ -1,8 +1,19 @@
-// Leetcode question 83
+// Leetcode question 83: Remove Duplicates from Sorted List
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
 // Solve using a linked-list
 
-var deleteDuplicates = function(head) {
-    let cur = head;
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var deleteDuplicates = function(head) {
     
 };
 
