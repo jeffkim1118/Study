51cb670552dd4338fdfb70c012ad141e57824f40
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-12-06 17:08:43 -0500

    updated em.js

--- javascriptStudy/em.js
+++ javascriptStudy/em.js
@@ -1,41 +1,41 @@
 //anagram checker
-let string1 = 'abbvccccde'
-let string2 = 'cdabc'
+// let string1 = 'abbvccccde'
+// let string2 = 'cdabc'
 
-let obj1 = {}
-let obj2 = {}
+// let obj1 = {}
+// let obj2 = {}
 
-for(let i = 0; i < string1.length; i++){
-    if(!obj1[string1[i]]) obj1[string1[i]] = 0;
-    obj1[string1[i]]++;
-}
+// for(let i = 0; i < string1.length; i++){
+//     if(!obj1[string1[i]]) obj1[string1[i]] = 0;
+//     obj1[string1[i]]++;
+// }
 
-for(let j = 0; j < string2.length; j++){
-    if(!obj2[string2[j]]) obj2[string2[j]] = 0;
-    obj2[string2[j]]++;
-}
+// for(let j = 0; j < string2.length; j++){
+//     if(!obj2[string2[j]]) obj2[string2[j]] = 0;
+//     obj2[string2[j]]++;
+// }
 
-Object.keys(obj1).map((char) => obj1[char] === obj2[char])
-let longest = 0;
-let longestVal;
-let result = '';
-let arr = [];
+// Object.keys(obj1).map((char) => obj1[char] === obj2[char])
+// let longest = 0;
+// let longestVal;
+// let result = '';
+// let arr = [];
 
-Object.keys(obj1).sort((first, sec) => obj1[first] - obj1[sec]).filter((i) => {
-    if(longest < obj1[i]){
-        longest=obj1[i]
-    }
-    longestVal = Math.max(0,longest)
+// Object.keys(obj1).sort((first, sec) => obj1[first] - obj1[sec]).filter((i) => {
+//     if(longest < obj1[i]){
+//         longest=obj1[i]
+//     }
+//     longestVal = Math.max(0,longest)
 
-    if(obj1[i] === longestVal){
-        result = i
-    }
-})
+//     if(obj1[i] === longestVal){
+//         result = i
+//     }
+// })
 
-console.log(Object.keys(obj1).sort((first, sec) => obj1[sec] - obj1[first]))
+// console.log(Object.keys(obj1).sort((first, sec) => obj1[sec] - obj1[first]))
 
 
-console.log(result)
+// console.log(result)
 
 
 
@@ -46,43 +46,43 @@ console.log(result)
 // character counter
 // 'agafsggas'
 
-function charCounter(str){
-    let obj = {}
+// function charCounter(str){
+//     let obj = {}
 
-    for(let i = 0; i < str.length; i++){
-        if(!obj[str[i]]) obj[str[i]] = []
-        obj[str[i]].push(str[i])
-    }
-    let largest = 0
-    Object.values(obj).forEach((array) => {
-        if(largest < array.length){largest=array.length}
-    })
-}
-let str = 'baddfbasdf'
-console.log(charCounter(str))
-
-
-
-
-// Reverse string
-
-function reverseStr(str){
-
-}
-
-console.log(reverseStr('h35h4ws5try5$^##'))
+//     for(let i = 0; i < str.length; i++){
+//         if(!obj[str[i]]) obj[str[i]] = []
+//         obj[str[i]].push(str[i])
+//     }
+//     let largest = 0
+//     Object.values(obj).forEach((array) => {
+//         if(largest < array.length){largest=array.length}
+//     })
+// }
+// let str = 'baddfbasdf'
+// console.log(charCounter(str))
 
 
 
 
+// // Reverse string
 
-// Palindrome
+// function reverseStr(str){
 
-function palin(str){
-    let reversed = str.split('').reverse().join('')
-    return str === reversed
-}
-console.log(palin('jhaltdjhlajwog'))
+// }
+
+// console.log(reverseStr('h35h4ws5try5$^##'))
+
+
+
+
+
+// // Palindrome
+
+// function palin(str){
+//     let reversed = str.split('').reverse().join('')
+//     return str === reversed
+// }
+// console.log(palin('jhaltdjhlajwog'))
 
 
 
@@ -109,7 +109,9 @@ Explanation: "leeto" did not occur in "leetcode", so we return -1.
 
 */
 function findNeedle(haystack, needle){
-    return haystack.indexOf(needle)
+    // return haystack.indexOf(needle)
+    return haystack.includes(needle)
+    
 }
 console.log(findNeedle("haegbsadbww", 'sad'))
 
@@ -171,8 +173,28 @@ console.log(caesarsCipher())
 
 
 
-function cashRegister(price, payment, cid){
+/*
+Design a cash register drawer function checkCashRegister() that 
+accepts purchase price as the first argument (price), 
+payment as the second argument (cash), 
+and cash-in-drawer (cid) as the third argument.
 
+cid is a 2D array listing available currency.
+
+The checkCashRegister() function should always return an object with a status key and a change key.
+
+Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, 
+or if you cannot return the exact change.
+
+Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change 
+if it is equal to the change due.
+
+Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, 
+sorted in highest to lowest order, as the value of the change key.
+*/
+
+function cashRegister(price, payment, cid){
+    
 }
 //Arguments: price, payment, cash-in-drawer
 console.log(cashRegister(11.75, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
